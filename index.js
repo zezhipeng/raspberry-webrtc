1c7c477cec93efc9d60709872b067ef597760458
zezhipeng <372962347@qq.com>
2017-07-13 23:39:53 +0800

    remove

--- index.js
+++
@@ -1,8 +0,0 @@
-var raspivid = require('raspivid')
-var fs = require('fs')
-var {resolve} = require('path')
-
-var file = fs.createWriteStream(resolve(process.cwd(), './video.h264'))
-var video = raspivid()
-console.log('test2')
-video.pipe(file)
