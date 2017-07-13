cc99c7fd587ddc9a44e2f089a8b45c269a612a8b
zezhipeng <372962347@qq.com>
2017-07-13 10:47:34 +0800

    video

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// var raspivid = require('raspivid')
-// var fs = require('fs')
-// var {resolve} = require('path')
+var raspivid = require('raspivid')
+var fs = require('fs')
+var {resolve} = require('path')
 
-// var file = fs.createWriteStream(resolve(__dirname, '/video.h264'))
-// var video = raspivid()
-// console.log('test2')
-// video.pipe(file)
-
-console.log(233)
+var file = fs.createWriteStream(resolve(__dirname, '/video.h264'))
+var video = raspivid()
+console.log('test2')
+video.pipe(file)
