7bc3bde21a2dbb36839e54c2b902fc85fb2b01cb
zezhipeng <372962347@qq.com>
2017-07-13 11:29:02 +0800

    ppp

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var raspivid = require('raspivid')
 var fs = require('fs')
 var {resolve} = require('path')
 
-var file = fs.createWriteStream(resolve(__dirname, '/video.h264'))
+var file = fs.createWriteStream(resolve(process.cwd(), './video.h264'))
 var video = raspivid()
 console.log('test2')
 video.pipe(file)
