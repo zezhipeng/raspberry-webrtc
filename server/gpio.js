ff66f0696deaf97ceb5c211079ddf1975d90fb73
zezhipeng <372962347@qq.com>
2017-07-22 21:43:45 +0800

    rm console.log

--- server/gpio.js
+++ server/gpio.js
@@ -4,7 +4,6 @@ const SYS_PATH = '/sys/class/gpio/gpio'
 
 export const open = (pinNumber, direction) => new Promise((resolve, reject) => {
   const path = `${SYS_PATH}${pinNumber}/direction`
-  console.log('open ', path)
   writeFile(path, direction, 'utf8', (err, res) => {
     if (err) reject(err)
     resolve(res)
@@ -13,7 +12,6 @@ export const open = (pinNumber, direction) => new Promise((resolve, reject) => {
 
 export const write = (pinNumber, value) => new Promise((resolve, reject) => {
   const path = `${SYS_PATH}${pinNumber}/value`
-  console.log('write ', path)
   writeFile(path, value, 'utf8', (err, res) => {
     if (err) reject(err)
     resolve(res)
